352880baa2d390f09f19b891da6a772ed53b058e
Daehwan2 <dhdh0482@naver.com>
2021-10-09 22:07:41 +0900

    🚧 Import tiny-slider.js and initialize product carousel

---
+++ js/productCarousel.js
@@ -0,0 +1,11 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  preventScrollOnTouch: true,
+});
