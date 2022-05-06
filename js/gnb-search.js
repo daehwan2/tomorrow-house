845ce688586e694ddd206a7e00844b16a892afe4
daehwan2 <dhdh0482@naver.com>
2022-05-06 14:02:52 +0900

    🚧 Implement deleting all search-historys

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search');
 const gnbSearchInput = gnbSearch.querySelector('input');
 const gnbSearchHistory = gnbSearch.querySelector('.search-history');
+const gnbSearchHistoryList = gnbSearch.querySelector('ol');
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+);
 
 const closeGnbSearchHistoryOnClickOutside = (e) => {
   if (!gnbSearch.contains(e.target)) {
@@ -9,6 +14,9 @@ const closeGnbSearchHistoryOnClickOutside = (e) => {
   }
 };
 const openGnbSearchHistory = () => {
+  if (gnbSearchHistoryList.children.length === 0) {
+    return;
+  }
   if (!gnbSearchHistory.classList.contains('is-actve')) {
     window.addEventListener('click', closeGnbSearchHistoryOnClickOutside);
   }
@@ -16,3 +24,10 @@ const openGnbSearchHistory = () => {
 };
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory);
+
+const deleteAllSearchHistoryItems = () => {
+  gnbSearchHistoryList.innerHTML = '';
+  gnbSearchHistory.classList.remove('is-active');
+};
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems);
